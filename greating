3441afa2240975e47ec0cb0a0474405594b8c03d
EstifanosZ <estifanoszerihun3@gmail.com>
2022-12-29 16:30:28 +0300

    edited by estifanos

---
+++ greating
@@ -0,0 +1,7 @@
+print("Enter you name")
+c=input()
+names=["Mikias shimeless","Estifanos zerihun","ermias lishan"]
+names.append(c)
+def demo(names):
+   print("Hello "+names)
+demo(c)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+a=int(input())
+print("Enter the second number")
+b=int(input())
+sum=a+b
+print("sum = ",sum)
+print(ord("A"))
+txt="hello I am {:s} I am {:d} years old".format("biruk", 22)
+print(txt)
