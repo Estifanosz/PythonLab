b46b40fe304c098234b6dd4116370909ab430c0c
Mikyas Shimeles H <121050898+imOmnipotent@users.noreply.github.com>
2023-01-26 20:40:57 +0300

    Update Group3.py

--- AI group/Group3.py
+++ AI group/Group3.py
@@ -1,22 +1,22 @@
-import re
+import  re
 print("Enter 1 for English")
 print("Enter 2 for Amharic")
 print("Enter 3 to Exit")
-choice=int(input())
+choice = int(input())
 match choice:
    case 1:
       file = open("Group3.txt","r")
-      string=file.read()
-      newstring=re.sub('[^a-zA-Z0-9\n\\s]','',string)
+      string = file.read()
+      newstring = re.sub('[^a-zA-Z0-9\n\\s]','',string)
       print(newstring)
       file.close()  
       file1 = open("Group3.txt","w")
       file1.write(newstring)
       file1.close()
       def charfreq():
-         dic={}
+         dic = {}
          file2 = open("Group3.txt","r")
-         string1=file2.read()
+         string1 = file2.read()
          
          for ch in string1:
             if ch.isspace():
