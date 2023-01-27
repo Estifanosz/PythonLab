de9b1dec600267e3bdeb5aec9ab8506610027a81
Zlalem <121661788+Zlalem@users.noreply.github.com>
2023-01-27 20:55:51 +0300

    Update Group3_Assignment.py

--- AI group/Group3_Assignment.py
+++ AI group/Group3_Assignment.py
@@ -18,3 +18,21 @@ match choice:
             dic = {}
             file2 = open("Group3.txt", "r")
             string1 = file2.read()
+            
+            for ch in string1:
+                if ch.isspace():
+                    continue
+                if ch in dic:
+                    dic[ch] = dic[ch]+1
+                else:
+                    dic[ch] = 1
+            sorted_chars = sorted(
+                dic.items(), key=lambda x: x[1], reverse=True)
+            sorted_dic = dict(sorted_chars)
+            print("character", "        ", "Frequency")
+            for key in sorted_dic:
+                print("  ", key, "---------------->", sorted_dic[key])
+            print()
+            print("Top five frequently occuring characters")
+            print("character", "        ", "Frequency")
+            i = 1
